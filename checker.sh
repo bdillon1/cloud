f3c3a1d95d110ba019032e723c6d4c8e1375e74d
bdillon1 <benjamin.dillon@valpo.edu>
2017-02-20 20:23:51 -0600

    checker.sh

---
+++ checker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+#user and shell checker script, name it checker.sh
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 username shell"
+	exit 2
+else
+	user=$1
+	shell=$2
+	for x in $(cat /etc/passwd)
+	do
+		un=$(echo $x | grep $user)
+		if [ $? -ne 0 ]; then
+			echo “$user not yet found”
+		else
+			echo “$user found”
+			sh=$(echo $x| cut -d':' -f7 | cut -d'/' -f3)
+			if [ “$sh” = “$shell” ]; then
+				echo “shell is correct”
+			else “shell is incorrect”
+			fi
+		fi
+fi
+done 
